cdf69f7a6ec6257d289e515d257c849af1537c38
chiwei93 <chiwei0708@gmail.com>
2022-02-10 20:51:05 +0800

    add page transition to the prize page

--- pages/prizes/[id].js
+++ pages/prizes/[id].js
@@ -1,5 +1,7 @@
+import { useRef } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { CSSTransition } from 'react-transition-group';
 
 import prizeData from '../../utils/prizes';
 
@@ -9,7 +11,10 @@ const Prize = () => {
   const router = useRouter();
   const { id } = router.query;
 
-	// if the page is reloaded
+  // ref for the container for the css transition group
+  const divElement = useRef(null);
+
+  // if the page is reloaded
   if (!id) {
     return (
       <div
@@ -27,15 +32,26 @@ const Prize = () => {
   };
 
   return (
-    <div
-      className={styles.container}
-      style={{ backgroundImage: `url(${background()})` }}
-    >
-      <div className={styles.btnContainer}>
-        <Link href="/">
-          <a className={styles.btn}>RESTART</a>
-        </Link>
-      </div>
+    <div className={styles.outerContainer}>
+      <CSSTransition
+        in={true}
+        timeout={200}
+        nodeRef={divElement}
+        classNames="slide"
+        appear
+      >
+        <div
+          className={styles.container}
+          style={{ backgroundImage: `url(${background()})` }}
+          ref={divElement}
+        >
+          <div className={styles.btnContainer}>
+            <Link href="/">
+              <a className={styles.btn}>RESTART</a>
+            </Link>
+          </div>
+        </div>
+      </CSSTransition>
     </div>
   );
 };
